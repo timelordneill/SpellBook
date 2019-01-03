5bb478932f2b22a4348054451ddcf4477fb88342
timelordneill <neill.wauters@gmail.com>
2019-01-04 00:13:40 +0100

    updated add spellbook layout

--- app/src/main/java/com/example/spellbook/Fragments/AddSpellbookFragment.kt
+++ app/src/main/java/com/example/spellbook/Fragments/AddSpellbookFragment.kt
@@ -1,17 +1,31 @@
 package com.example.spellbook.Fragments
 
+import android.app.ActionBar
 import android.content.Context
 import android.net.Uri
+import android.opengl.Visibility
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.PopupWindow
+import android.widget.Spinner
 
 import com.example.spellbook.R
+import com.example.spellbook.domain.*
+import kotlinx.android.synthetic.main.fragment_add_spellbook.*
+import kotlinx.android.synthetic.main.fragment_spell_list.*
+import kotlinx.android.synthetic.main.popup_add_class.view.*
 
 
 class AddSpellbookFragment : Fragment() {
+
+    private lateinit var spellbook:Spellbook
+    private var classes= mutableListOf<CharacterClass>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +41,29 @@ class AddSpellbookFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
+
+        class_recyclerview.adapter = ClassRecyclerViewAdapter(this, classes)
+        class_recyclerview.layoutManager= LinearLayoutManager(activity)
+
+        //set popup to open when button is clicked
+        val view=layoutInflater.inflate(R.layout.popup_add_class, null)
+        var popup=PopupWindow(view, ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT)
+
+        view.class_spinner.adapter=ArrayAdapter<Classes>(context, android.R.layout.simple_spinner_item, Classes.values())
+
+        view.level_np.maxValue=20
+        view.level_np.minValue=1
+
+        view.add_class_button.setOnClickListener {
+            val characterClass= view.class_spinner.selectedItem as Classes
+            classes.add(CharacterClass(characterClass, view.level_np.value))
+            class_recyclerview.adapter = ClassRecyclerViewAdapter(this, classes)
+            class_recyclerview.layoutManager= LinearLayoutManager(activity)
+            popup.dismiss()
+        }
+
+        add_class_button.setOnClickListener {
+            popup.showAsDropDown(spellbook_constraint, 10, 10)
+        }
     }
 }
---
+++ app/src/main/java/com/example/spellbook/domain/ClassRecyclerViewAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.spellbook.domain
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.spellbook.Activities.MainActivity
+import com.example.spellbook.Fragments.AddSpellbookFragment
+import com.example.spellbook.Fragments.SpellListFragment
+import com.example.spellbook.R
+import kotlinx.android.synthetic.main.spell_list_content.view.*
+import kotlinx.android.synthetic.main.spellbook_list_content.view.*
+
+class ClassRecyclerViewAdapter(private val parentActivity: AddSpellbookFragment,
+                               private val classes: List<CharacterClass>) :
+    RecyclerView.Adapter<ClassRecyclerViewAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.spellbook_list_content, parent, false)
+        return ViewHolder(view)
+    }
+
+    /**
+     * fills the items in the list
+     * Puts Cantrip in stead of lvl 0 spell
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val characterClass = classes[position]
+        holder.name.text = characterClass.name.className
+
+        when {
+            characterClass.name == Classes.Bard -> holder.classImage.setImageResource(R.drawable.bard)
+            characterClass.name == Classes.Sorcerer -> holder.classImage.setImageResource(R.drawable.sorcerer)
+            characterClass.name == Classes.Cleric -> holder.classImage.setImageResource(R.drawable.cleric)
+            characterClass.name == Classes.Druid -> holder.classImage.setImageResource(R.drawable.druid)
+            characterClass.name == Classes.Ranger -> holder.classImage.setImageResource(R.drawable.ranger)
+            characterClass.name == Classes.Paladin -> holder.classImage.setImageResource(R.drawable.paladin)
+            characterClass.name == Classes.Warlock -> holder.classImage.setImageResource(R.drawable.warlock)
+            characterClass.name == Classes.Wizard -> holder.classImage.setImageResource(R.drawable.wizard)
+        }
+    }
+
+    override fun getItemCount() = classes.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val name: TextView = view.text_charactername
+        val classImage: de.hdodenhof.circleimageview.CircleImageView = view.class_image
+    }
+
+}
